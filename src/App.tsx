11ce07f7d22abc83e4354dfe58f5eb60ab986ac3
Ivan Amoshyi <amoshyiivan@gmail.com>
2025-08-30 13:30:02 +0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='bg-bg text-2xl text-accent'>This is Your Gym Notes App</div>
+    </>
+  )
+}
+
+export default App;

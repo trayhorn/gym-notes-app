33b4fa38dfadc2de5b7624857eb504e80a693e7d
trayhorn <amoshyiivan@gmail.com>
2026-07-13 19:43:52 +0300

    Added No workouts indication in the HomePage

---
+++ src/components/NoWorkouts.tsx
@@ -0,0 +1,7 @@
+export default function NoWorkouts() {
+  return (
+    <div className="text-center mt-8">
+      <p className="text-[2rem] text-gray-500">No workouts found.</p>
+    </div>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,47 +3,46 @@ import { fetchAllWorkouts } from "../api";
 import { useQuery } from "@tanstack/react-query";
 import Loader from "../components/Loader";
 import { Link, useLocation } from "react-router";
+import NoWorkouts from "../components/NoWorkouts";
 
 function App() {
-	const {
-		isPending,
-		isError,
-		isSuccess,
-		data: workouts,
-		error,
-	} = useQuery({
-		queryKey: ["workouts"],
-		queryFn: fetchAllWorkouts,
-		enabled: !!localStorage.getItem("authToken"),
-	});
+  const {
+    isPending,
+    isError,
+    isSuccess,
+    data: workouts,
+    error,
+  } = useQuery({
+    queryKey: ["workouts"],
+    queryFn: fetchAllWorkouts,
+    enabled: !!localStorage.getItem("authToken"),
+  });
 
-	const location = useLocation();
+  const location = useLocation();
 
-	localStorage.setItem("lastVisitedPage", location.pathname);
+  localStorage.setItem("lastVisitedPage", location.pathname);
 
-	if (isPending) return <Loader />;
-
-	if (isError) {
-		return <span>Error: {error.message}</span>;
-	}
-
-	return (
-		<>
-			{isPending && <Loader />}
-			{isError && <span>Something went wrong</span>}
-			{isSuccess && (
-				<>
-					<Link
-						to="/add_workout"
-						className="bg-primary text-text-secondary py-2 px-4 inline-block w-full text-center"
-					>
-						Add Workout
-					</Link>
-					<WorkoutGallery workouts={workouts} />
-				</>
-			)}
-		</>
-	);
+  return (
+    <>
+      {isPending && <Loader />}
+      {isError && <span>Error: {error.message}</span>}
+      {isSuccess && (
+        <>
+          <Link
+            to="/add_workout"
+            className="bg-primary text-text-secondary py-2 px-4 inline-block w-full text-center"
+          >
+            Add Workout
+          </Link>
+          {workouts.length > 0 ? (
+            <WorkoutGallery workouts={workouts} />
+          ) : (
+            <NoWorkouts />
+          )}
+        </>
+      )}
+    </>
+  );
 }
 
 export default App;
